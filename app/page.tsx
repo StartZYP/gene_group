7888b7077f248c48362aaa2882846c58f58944a7
root <root@localhost.localdomain>
2024-10-14 13:04:28 +0800

    gene文档初始化

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+/*
+ * @Author: StartZyp
+ * @Date: 2024-10-14 12:45:24
+ * @LastEditors: StartZyp
+ * @LastEditTime: 2024-10-14 12:45:24
+ * @FilePath: /gene_group/app/page.tsx
+ * 
+ * Copyright (c) 2024 by StartZyp/Reddrip, All Rights Reserved. 
+ */
 import Link from "next/link";
 
 import { buttonVariants } from "@/components/ui/button";
@@ -7,17 +16,17 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Documents
+        宿命重载
       </h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple to use and easy to customize.
+        Gene插件文档大全. 仅限内部团队成员使用&apos;.
       </p>
       <div className="flex items-center gap-5">
         <Link
           href={`/docs${PageRoutes[0].href}`}
           className={buttonVariants({ className: "px-6", size: "lg" })}
         >
-          Get Started
+          快速开始
         </Link>
       </div>
     </div>
--- dist/scripts/settings/documents.mjs
+++ dist/scripts/settings/documents.mjs
@@ -1,91 +1,89 @@
+/*
+ * @Author: StartZyp
+ * @Date: 2024-10-14 12:46:19
+ * @LastEditors: StartZyp
+ * @LastEditTime: 2024-10-14 13:01:00
+ * @FilePath: /gene_group/dist/scripts/settings/documents.mjs
+ * 
+ * Copyright (c) 2024 by StartZyp/Reddrip, All Rights Reserved. 
+ */
 export const Documents = [
     {
         title: "Introduction",
         href: "/introduction",
-        heading: "Getting started",
+        heading: "宿命重载插件文档",
         items: [
             {
-                title: "Installation",
+                title: "[Dp附属]Gene_Dplus",
                 href: "/installation",
             },
             {
-                title: "Setup",
+                title: "[宝箱插件]Gene_TreasureBox",
                 href: "/setup",
             },
             {
-                title: "Changelog",
+                title: "[chemdah附属]Gene_TaskView",
+                href: "/changelog",
+            },
+            {
+                title: "[电梯插件]Gene_Elevator",
+                href: "/changelog",
+            },
+            {
+                title: "[电梯插件]Gene_Elevator",
+                href: "/changelog",
+            },
+            {
+                title: "[生涯等级]Gene_CareerLevel",
+                href: "/changelog",
+            },
+            {
+                title: "[角色系统]Gene_Roles",
+                href: "/changelog",
+            },
+            {
+                title: "[采集模块]Gene_Roles",
+                href: "/changelog",
+            },
+            {
+                title: "[背包模块]Gene_Inventory",
+                href: "/changelog",
+            },
+            {
+                title: "[强化模块]Gene_Strengthen",
+                href: "/changelog",
+            },
+            {
+                title: "[多经济模块]Gene_Inventory",
+                href: "/changelog",
+            },
+            {
+                title: "[组队模块]Gene_BungeeTeam",
+                href: "/changelog",
+            },
+            {
+                title: "[跨服系统]Gene_ServerChange",
+                href: "/changelog",
+            },
+            {
+                title: "[物品管理模块]Gene_ItemPlant",
+                href: "/changelog",
+            },
+            {
+                title: "[图纸系统]Gene_BlueprintCraft",
+                href: "/changelog",
+            },
+            {
+                title: "[护盾模块]Gene_Shield",
+                href: "/changelog",
+            },
+            {
+                title: "[主手模块]Gene_MainHand",
                 href: "/changelog",
             },
         ],
     },
     {
         spacer: true,
-    },
-    {
-        title: "Navigation",
-        href: "/navigation",
-        heading: "Documents",
-    },
-    {
-        title: "Structure",
-        href: "/structure",
-        items: [
-            {
-                title: "Deeper",
-                href: "/deeper",
-                items: [
-                    {
-                        title: "Even deeper",
-                        href: "/even-deeper",
-                    },
-                ]
-            },
-        ],
-    },
-    {
-        spacer: true,
-    },
-    {
-        title: "Markup",
-        href: "/markup",
-        heading: "Components",
-        items: [
-            {
-                title: "Cards",
-                href: "/cards",
-            },
-            {
-                title: "Diagrams",
-                href: "/diagrams",
-            },
-            {
-                title: "Filetree",
-                href: "/filetree",
-            },
-            {
-                title: "Lists",
-                href: "/lists",
-            },
-            {
-                title: "Maths",
-                href: "/maths",
-            },
-            {
-                title: "Notes",
-                href: "/notes",
-            },
-            {
-                title: "Steps",
-                href: "/steps",
-            },
-            {
-                title: "Table",
-                href: "/table",
-            },
-            {
-                title: "Tabs",
-                href: "/tabs",
-            },
-        ],
-    },
+    }
 ];

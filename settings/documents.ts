0d3e3739f1b1838cf054789c99cea1cbd72d5ed7
root <root@localhost.localdomain>
2024-10-14 13:10:27 +0800

    初始化目录

--- settings/documents.ts
+++ settings/documents.ts
@@ -1,93 +1,91 @@
+/*
+ * @Author: StartZyp
+ * @Date: 2024-10-14 13:10:12
+ * @LastEditors: StartZyp
+ * @LastEditTime: 2024-10-14 13:10:12
+ * @FilePath: /gene_group/settings/documents.ts
+ * 
+ * Copyright (c) 2024 by StartZyp/Reddrip, All Rights Reserved. 
+ */
 import { Paths } from "@/lib/pageroutes";
 
 export const Documents: Paths[] = [
-  {
-    title: "Introduction",
-    href: "/introduction",
-    heading: "Getting started",
-    items: [
-      {
-        title: "Installation",
-        href: "/installation",
-      },
-      {
-        title: "Setup",
-        href: "/setup",
-      },
-      {
-        title: "Changelog",
-        href: "/changelog",
-      },
-    ],
-  },
-  {
-    spacer: true,
-  },
-  {
-    title: "Navigation",
-    href: "/navigation",
-    heading: "Documents",
-  },
-  {
-    title: "Structure",
-    href: "/structure",
-    items: [
-      {
-        title: "Deeper",
-        href: "/deeper",
-        items : [
+    {
+      title: "Introduction",
+      href: "/introduction",
+      heading: "宿命重载插件文档",
+      items: [
           {
-            title: "Even deeper",
-            href: "/even-deeper",
+              title: "[Dp附属]Gene_Dplus",
+              href: "/installation",
           },
-        ]
-      },
-    ],
-  },
-  {
-    spacer: true,
-  },
-  {
-    title: "Markup",
-    href: "/markup",
-    heading: "Components",
-    items: [
-      {
-        title: "Cards",
-        href: "/cards",
-      },
-      {
-        title: "Diagrams",
-        href: "/diagrams",
-      },
-      {
-        title: "Filetree",
-        href: "/filetree",
-      },
-      {
-        title: "Lists",
-        href: "/lists",
-      },
-      {
-        title: "Maths",
-        href: "/maths",
-      },
-      {
-        title: "Notes",
-        href: "/notes",
-      },
-      {
-        title: "Steps",
-        href: "/steps",
-      },
-      {
-        title: "Table",
-        href: "/table",
-      },
-      {
-        title: "Tabs",
-        href: "/tabs",
-      },
-    ],
-  },
+          {
+              title: "[宝箱插件]Gene_TreasureBox",
+              href: "/setup",
+          },
+          {
+              title: "[chemdah附属]Gene_TaskView",
+              href: "/changelog",
+          },
+          {
+              title: "[电梯插件]Gene_Elevator",
+              href: "/changelog",
+          },
+          {
+              title: "[电梯插件]Gene_Elevator",
+              href: "/changelog",
+          },
+          {
+              title: "[生涯等级]Gene_CareerLevel",
+              href: "/changelog",
+          },
+          {
+              title: "[角色系统]Gene_Roles",
+              href: "/changelog",
+          },
+          {
+              title: "[采集模块]Gene_Roles",
+              href: "/changelog",
+          },
+          {
+              title: "[背包模块]Gene_Inventory",
+              href: "/changelog",
+          },
+          {
+              title: "[强化模块]Gene_Strengthen",
+              href: "/changelog",
+          },
+          {
+              title: "[多经济模块]Gene_Inventory",
+              href: "/changelog",
+          },
+          {
+              title: "[组队模块]Gene_BungeeTeam",
+              href: "/changelog",
+          },
+          {
+              title: "[跨服系统]Gene_ServerChange",
+              href: "/changelog",
+          },
+          {
+              title: "[物品管理模块]Gene_ItemPlant",
+              href: "/changelog",
+          },
+          {
+              title: "[图纸系统]Gene_BlueprintCraft",
+              href: "/changelog",
+          },
+          {
+              title: "[护盾模块]Gene_Shield",
+              href: "/changelog",
+          },
+          {
+              title: "[主手模块]Gene_MainHand",
+              href: "/changelog",
+          },
+      ],
+    },
+    {
+        spacer: true,
+    },
 ];

fb8bba9e149533a410ba5284ec4d1eddaa3a7aee
root <root@localhost.localdomain>
2024-10-14 13:20:38 +0800

    更新title导航栏

--- settings/settings.ts
+++ settings/settings.ts
@@ -1,10 +1,19 @@
+/*
+ * @Author: StartZyp
+ * @Date: 2024-10-14 13:20:28
+ * @LastEditors: StartZyp
+ * @LastEditTime: 2024-10-14 13:20:28
+ * @FilePath: /gene_group/settings/settings.ts
+ * 
+ * Copyright (c) 2024 by StartZyp/Reddrip, All Rights Reserved. 
+ */
 export const url = "https://rubix-documents.vercel.app";
 export const siteicon = "/icon.png";
 
 export const gtm = "GTM-XXXXXXX";
 export const gtmconnected = true;
 
-export const sitename = "Documents";
+export const sitename = "Gene项目插件文档";
 export const description = "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.";
 export const keywords = ["Next.js", "documentation", "Rubix Studios", "open source", "SEO"];
 export const urlimage = "/images/og-image.png";

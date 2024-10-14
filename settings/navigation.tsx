3d4ec5b9835b9616a6e5e64c2bb120bab5c536aa
root <root@localhost.localdomain>
2024-10-14 13:18:32 +0800

    更新title导航栏

--- settings/navigation.tsx
+++ settings/navigation.tsx
@@ -1,17 +1,31 @@
+/*
+ * @Author: StartZyp
+ * @Date: 2024-10-14 13:15:44
+ * @LastEditors: StartZyp
+ * @LastEditTime: 2024-10-14 13:17:47
+ * @FilePath: /gene_group/settings/navigation.tsx
+ * 
+ * Copyright (c) 2024 by StartZyp/Reddrip, All Rights Reserved. 
+ */
 import { PageRoutes } from "@/lib/pageroutes";
 
 export const Navigations = [
   {
-    title: "Docs",
+    title: "插件列表",
     href: `/docs${PageRoutes[0].href}`,
   },
   {
-    title: "Home",
-    href: "https://www.rubixstudios.com.au/",
+    title: "项目主页",
+    href: "https://www.baidu.com/",
+    external: true,
+  },
+  {
+    title: "项目进度",
+    href: "https://www.baidu.com/",
     external: true,
   }
 ];
 
 export const GitHubLink = {
-  href: "https://github.com/rubixvi/rubix-documents",
+  href: "https://www.baidu.com/",
 };
